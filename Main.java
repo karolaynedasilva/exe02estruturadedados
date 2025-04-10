303d0149dd5d8733bb65462cd2c5742a0400f230
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2025-04-10 20:27:16 -0300

    feat: pilha e vetor

---
+++ Main.java
@@ -0,0 +1,36 @@
+import estruturas.ListaVetor;
+import estruturas.PilhaVetor;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaVetor listavetor = new ListaVetor();
+        PilhaVetor pilhavetor = new PilhaVetor();
+
+        listavetor.adicionar(1);
+        listavetor.adicionar(2);
+        listavetor.adicionar(3);
+        listavetor.adicionar(4);
+        listavetor.adicionar(5);
+        listavetor.adicionar(6);
+        listavetor.adicionar(7);
+        listavetor.adicionar(8);
+        listavetor.adicionar(9);
+        listavetor.adicionar(10);
+
+        System.out.println("-----");
+        System.out.println("Tamanho da lista: " + listavetor.getTamanho()); 
+        System.out.println("Elementos da lista: " );
+        listavetor.mostrarElementos();
+        System.out.println("-----");
+
+       pilhavetor.empilhar(1);
+       pilhavetor.empilhar(2);
+       pilhavetor.empilhar(3);
+       pilhavetor.empilhar(4);
+
+       System.out.println("------");
+
+        // OBSERVÇÃO AJUSTAR LÓGICA DO EMPILHAR 
+
+    }
+}
---
+++ estruturas/PilhaVetor.java
@@ -0,0 +1,69 @@
+package estruturas;
+
+public class PilhaVetor {
+    private int[] elementos;
+    private int topo;
+    private int capacidade;
+
+    public int[] getElementos() {
+        return elementos;
+    }
+
+    public void setElementos(int[] elementos) {
+        this.elementos = elementos;
+    }
+
+    public int getTopo() {
+        return topo;
+    }
+
+    public void setTopo(int topo) {
+        this.topo = topo;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void mostrarElementos(){
+        if (topo == -1) {
+            System.out.println("A lista está vazia");
+        }else{
+            for(int i =0; i<= topo; i++){
+                System.out.println(elementos[i] + " ");
+            }
+        }
+    }
+
+    public PilhaVetor(){
+        this.capacidade =10;
+        this.elementos = new int[this.capacidade];
+        this.topo = -1;
+    }
+
+    public void aumentarCapacidade(){
+        int novaCapacidade = this.capacidade * (this.capacidade/2);
+        int[] novoVetor = new int[novaCapacidade];
+
+        for(int i =0; i<= topo; i++){
+            novoVetor[i] = elementos[i];
+        }
+        elementos = novoVetor;
+        this.capacidade = novaCapacidade;
+    }
+
+    public void empilhar(int elemento){
+        if (topo >= capacidade -1) {
+            aumentarCapacidade();
+        }
+        topo++;
+        elementos[topo] = elemento;
+
+        System.out.println("Elemento empilhado: " + elemento);
+        System.out.println("Novo topo: " + topo);
+    }
+}
